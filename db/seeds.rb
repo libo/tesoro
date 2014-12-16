c87efb6d233d291477bf48abaced9517123a2ad8
Libo Cannici <lcannici@zendesk.com>
2014-12-16 01:54:30 +0100

    fix seeding with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,15 @@ Event.destroy_all
 zen = Stock.where(symbol: 'ZEN').first
 user = User.last
 
-user.events.create(stock: zen, action: :buy, quantity: 100, price: 95, executed_on: 10.days.ago)
-user.events.create(stock: zen, action: :buy, quantity: 200, price: 105, executed_on: 9.days.ago)
-user.events.create(stock: zen, action: :buy, quantity: 100, price: 107, executed_on: 8.days.ago)
+if user.present?
+  user.events.create(stock: zen, action: :buy, quantity: 100, price: 95, executed_on: 10.days.ago)
+  user.events.create(stock: zen, action: :buy, quantity: 200, price: 105, executed_on: 9.days.ago)
+  user.events.create(stock: zen, action: :buy, quantity: 100, price: 107, executed_on: 8.days.ago)
 
-user.events.create(stock: zen, action: :sell, quantity: 150, price: 110, executed_on: 5.days.ago)
+  user.events.create(stock: zen, action: :sell, quantity: 150, price: 110, executed_on: 5.days.ago)
 
-user.events.create(stock: zen, action: :buy, quantity: 50, price: 100, executed_on: 4.days.ago)
-user.events.create(stock: zen, action: :buy, quantity: 300, price: 107.5, executed_on: 3.days.ago)
+  user.events.create(stock: zen, action: :buy, quantity: 50, price: 100, executed_on: 4.days.ago)
+  user.events.create(stock: zen, action: :buy, quantity: 300, price: 107.5, executed_on: 3.days.ago)
 
-user.events.create(stock: zen, action: :sell, quantity: 200, price: 108, executed_on: 2.days.ago)
+  user.events.create(stock: zen, action: :sell, quantity: 200, price: 108, executed_on: 2.days.ago)
+end
