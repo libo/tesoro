da3c23d698874ff9f8e4476661b7f00d386bb7c2
Libo Cannici <lcannici@zendesk.com>
2014-12-17 00:54:44 +0100

    With import

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,3 +1,5 @@
+require 'event_importer'
+
 class ImportsController < ApplicationController
   respond_to :html, :csv
 
@@ -5,5 +7,16 @@ class ImportsController < ApplicationController
     @events = current_user.events.order(:executed_on).all
 
     respond_with(@events)
+  end
+
+  def create
+    if params[:wipe_all_exiting_event] == '1'
+      current_user.events.destroy_all
+    end
+
+    EventImporter.import(params[:file].path, current_user.email)
+
+    flash[:notice] = 'Data was successfully imported.'
+    render :index
   end
 end
