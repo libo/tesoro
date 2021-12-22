9d3dc0defbe1425b4a502dabfe71ea4964dee13d
Libo Cannici <lcannici@zendesk.com>
2021-12-22 14:42:00 +0100

    zeitwerk and 6.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Tesoro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 6.1
+    config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
     #
