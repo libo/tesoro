e654b32f3470e35cffbe5a0db9bc4b575dae4d7b
Libo Cannici <lcannici@zendesk.com>
2021-12-27 20:23:15 +0100

    Fix some cache collision and other issues by adding specs

--- spec/models/conversion_spec.rb
+++ spec/models/conversion_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Conversion do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    subject {
+      Conversion.new(book_on: DateTime.now,
+        currency: Currency.new(name: 'Euro', code: 'EUR'),
+        rate: 123)
+    }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a book_on" do
+      subject.book_on = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a currency" do
+      subject.currency = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a rate" do
+      subject.rate = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
