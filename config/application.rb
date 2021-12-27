4ff32fad36a2b66513c7bfdcaa69dfea028f817a
Libo Cannici <lcannici@zendesk.com>
2021-12-27 21:02:26 +0100

    Done app:update

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Tesoro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
     #
